a40c90ecf29b574e9b1092b97b89db7eececf550
KessNelly <ojkessnelly@gmail.com>
2024-07-02 13:00:19 +0100

    updated weather controller

--- controller/webController.js
+++ controller/webController.js
@@ -1,53 +1,49 @@
 const axios = require('axios');
 const fetchTemperature = require('../utils/weather');
 
+
 const getResponse = async (req, res) => {
     const visitorName = req.query.visitor_name;
 
     try {
-        //const clientIp = req.ip;
-        const clientIp = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-        console.log('Client IP:', clientIp);
+        let clientIp = process.env.NODE_ENV = "production" ? req.headers["x-forwarded-for"] : req.ip
+        clientIp = clientIp = "::1" ? "127.0.0.1" : clientIp
+        console.log(clientIp);
 
-        // Fetch location using ipinfo.io API
-        //const ipinfoUrl = `https://ipinfo.io/${clientIp}/json`;
-        const ipinfoUrl = `https://ipinfo.io/${clientIp}/json?token=${process.env.TOKEN}`;
-        console.log('ipinfoUrl:', ipinfoUrl);
+        // get location using a geolocation API
+       const ipAddress = await axios.get(`https://ipapi.co/${clientIp}/json/`);
 
+      // Check if ipAddress indicates an error
+       if (ipAddress.data.error) {
+        throw new Error(ipAddress.data.reason);
+         }
+       
+       const location = ipAddress.data.city;
 
-        const ipinfoResponse = await axios.get(ipinfoUrl);
-        console.log('ipinfoResponse:', ipinfoResponse.data);
-
-        if (!ipinfoResponse.data.city) {
-            throw new Error('Failed to fetch location');
-        
-        }
-        
-        const location = ipinfoResponse.data.city;
-        console.log('Location:', location);
-
-        // Get temperature from a weather API
+       console.log(ipAddress.data);
+        //get temperatue from a weather API
+       
         const temperature = await fetchTemperature(location);
 
         const greeting = `Hello, ${visitorName}!, the temperature is ${temperature} degrees Celsius in ${location}`;
 
         const data = {
-            client_ip: clientIp,
-            location: location,
-            greeting: greeting
-        };
-
-        res.json(data);
+                    client_ip: clientIp,
+                    location: location,
+                    greeting: greeting
+                };
+        
+            
+                res.json(data);
 
     } catch (error) {
         console.error('Error:', error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
+
 };
 
 module.exports = getResponse;
-
-
 
 
 
--- utils/weather.js
+++ utils/weather.js
@@ -1,27 +1,8 @@
-// const axios = require('axios');
-
-// const weatherApiKey = process.env.weatherApiKey;
-// const weatherUrl = process.env.weatherUrl;
-
-// const fetchWeather = async (req, res) =>{
-//     try {
-//         const response = await axios.get(weatherUrl);
-//         const weatherData = response.data;
-//         console.log(`Current temperature is ${weatherData.main.temp}°C`);
-//         // console.log(`Current temperature in ${city}: ${weatherData.main.temp}°C`);
-//         // console.log(`Weather description: ${weatherData.weather[0].description}`);
-//       } catch (error) {
-//         console.error('Error fetching weather data:', error.message);
-//       }
-// }
-
-// module.exports =  fetchWeather;
-
 const axios = require('axios');
 
 const weatherApiKey = process.env.weatherApiKey;
 const weatherUrl = process.env.weatherUrl;
-//console.log(weatherUrl);
+
 const fetchWeather = async (location) => {
     try {
         const response = await axios.get(weatherUrl, {
